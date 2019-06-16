4019ff905bd4cb4a2d7524c7f6e53c8cef9c20ea
joseliza <joseliza@iesciudadjardin.com>
2019-06-16 13:50:43 +0200

    Creando repo

---
+++ src/Cap1/Ejemplos_csv.py
@@ -0,0 +1,11 @@
+import csv
+
+with open('../../data/Cap1/subvenciones.csv', encoding='latin1') as fichero_csv:
+    lector = csv.reader(fichero_csv)
+    next(lector, None)  # Se salta la cabecera
+    importe_total = 0
+    for linea in lector:
+        importe_str = linea[2]
+        importe = float(importe_str)
+        importe_total = importe_total + importe
+    print(importe_total)
