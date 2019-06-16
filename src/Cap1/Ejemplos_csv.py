37a6c1c3cf3c26a9219b782ec803d2024278f5e9
joseliza <joseliza@iesciudadjardin.com>
2019-06-16 19:37:36 +0200

    Modificando

--- src/Cap1/Ejemplos_csv.py
+++ src/Cap1/Ejemplos_csv.py
@@ -1,11 +1,20 @@
 import csv
 
+# Las filas se tratan como listas
+# with open('../../data/Cap1/subvenciones.csv', encoding='latin1') as fichero_csv:
+#     lector = csv.reader(fichero_csv)
+#     next(lector, None)  # Se salta la cabecera
+#     importe_total = 0
+#     for linea in lector:
+#         importe = float(linea[2])
+#         importe_total = importe_total + importe
+#     print(importe_total)
+
+# Las filas se tratan como diccionarios
+
 with open('../../data/Cap1/subvenciones.csv', encoding='latin1') as fichero_csv:
-    lector = csv.reader(fichero_csv)
-    next(lector, None)  # Se salta la cabecera
-    importe_total = 0
-    for linea in lector:
-        importe_str = linea[2]
-        importe = float(importe_str)
-        importe_total = importe_total + importe
-    print(importe_total)
+    dic_lector = csv.DictReader(fichero_csv)
+    asocs = {}
+    for linea in dic_lector:
+        centro = linea['Asociación']
+        print(centro)
