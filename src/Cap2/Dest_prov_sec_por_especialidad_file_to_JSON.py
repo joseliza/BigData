1dab4e35b02b5b86412c504f83ea42c2cb9c0a9d
joseliza <joseliza@iesciudadjardin.com>
2019-08-17 20:52:11 +0200

    El script que va a escribir en formato json está casi listo. Arreglar apertura y cierre de corchetes y llave final (no debe llevar coma después).

--- src/Cap2/Dest_prov_sec_por_especialidad_file_to_JSON.py
+++ src/Cap2/Dest_prov_sec_por_especialidad_file_to_JSON.py
@@ -7,7 +7,7 @@ chrome_options.add_argument('--window-size=1920x1800')
 driver = webdriver.Chrome(options = chrome_options)
 
 # Página XX de la especialidad de matemáticas PES
-url = "file:///home/joseliza/Descargas/Matematicas-p18.html"
+url = "file:///home/joseliza/Descargas/Matematicas-p19.html"
 driver.get(url)
 
 # Obtenemos apellidos y nombres de todas las personas en la página
@@ -26,58 +26,71 @@ lista.pop(0)
 lista.pop(0)
 lista.pop(0)
 
-## PRUEBA PARA LA PRIMERA PERSONA
-persona = driver.find_element_by_link_text(lista[0])
-## Imprimo apellidos y nombre
-print (persona.text)
-
-# Entramos en el enlace que aparece en el nombre de la persona
-persona.click()
-
-# Extraemos colectivo
-colectivo = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[1]/div/div[2]/center[2]/table/tbody/tr/td/em")
-## Imprimo colectivo
-print (colectivo.text)
-
-# Extraemos tiempo de servicio
-tiempo = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[1]/div/div[2]/center[3]/table/tbody/tr/td")
-## Imprimo tiempo de servicio
-print (tiempo.text)
-
-# Extraemos cuerpo
-cuerpo = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[2]/div/div[2]/center/table/tbody/tr[1]/td")
-## Imprimo cuerpo
-print (cuerpo.text)
-
-# Extraemos puesto
-puesto = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[2]/div/div[2]/center/table/tbody/tr[2]/td")
-## Imprimo cuerpo
-print (puesto.text)
-
-# Extraemos centro
-centro = driver.find_elements_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[2]/div/div[2]/center/table/tbody/tr[3]/td/font")
-## Imprimo centro
-for i in centro:
-    print ('"' + i.text + '"')
-
-
-
 # Abrimos fichero para volcar los datos en él
-#f = open("/home/joseliza/Matematicas1.txt", "a")
+f = open("/home/joseliza/Matematicas1.json", "a")
 
-# count = 0
-# # Miramos la información extendida de cada persona (recorremos la lista de personas haciendo click en cada una):
-# for nombre in lista:
-#     print (len(lista)-count)
-#     count += 1
-#     persona = driver.find_element_by_link_text(nombre)
-#     persona.click()
-#     informacion = driver.find_elements_by_xpath("//*[@id='example']/tbody/tr")
-#     for extra in informacion:
-#         f.write(extra.text + os.linesep)
-#     driver.back()
+## Escribo apertura de corchete (debo hacerlo sólo si acabo de crear el fichero o si estaba vacío)
+## Por hacer
+f.write("[")
 
-#f.close()
+count = 0
+# Miramos la información extendida de cada persona (recorremos la lista de personas haciendo click en cada una):
+for nombre in lista:
+    print (len(lista)-count)
+    count += 1
+
+    # Escribo llave de apertura de campo
+    f.write("{")
+
+    persona = driver.find_element_by_link_text(nombre)
+    # Escribo apellidos y nombre en archivo
+    f.write ("Persona:"+'"'+persona.text+'",')
+
+    # Entramos en el enlace que aparece en el nombre de la persona
+    ##persona.click()
+
+    # Extraemos colectivo
+    colectivo=persona
+    ##colectivo = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[1]/div/div[2]/center[2]/table/tbody/tr/td/em")
+    # Escribo colectivo en archivo
+    f.write ("Colectivo:"+'"'+colectivo.text+'",')
+
+    # Extraemos tiempo de servicio
+    tiempo=persona
+    ##tiempo = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[1]/div/div[2]/center[3]/table/tbody/tr/td")
+    # Escribo tiempo de servicio en archivo
+    f.write ("Tiempo:"+'"'+tiempo.text+'",')
+
+    # Extraemos cuerpo
+    cuerpo=persona
+    ##cuerpo = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[2]/div/div[2]/center/table/tbody/tr[1]/td")
+    # Escribo cuerpo en archivo
+    f.write ("Cuerpo:"+'"'+cuerpo.text+'",')
+
+    # Extraemos puesto
+    puesto=persona
+    ##puesto = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[2]/div/div[2]/center/table/tbody/tr[2]/td")
+    # Escribo puesto en archivo
+    f.write ("Puesto:"+'"'+puesto.text+'",')
+
+    # Extraemos centro
+    centro=persona
+    ##centro = driver.find_elements_by_xpath("/html/body/div[3]/div[1]/div/div[2]/div[2]/div/div[2]/center/table/tbody/tr[3]/td/font")
+    # Escribo centro en archivo
+    ##for i in centro:
+    ##    f.write ("Centro:"+'"' + i.text + '"' + os.linesep)
+
+    # Escribo llave de cierre de campo
+    ## La coma no se escribe si es la última llave. Esto queda por hacer
+    f.write("},")
+
+    # Vuelvo a página anterior (lista de personas)
+    #driver.back()
+
+## Escribo cierre de corchete (debo hacerlo sólo si acabo de crear el fichero o si estaba vacío)
+## Por hacer
+f.write("]")
+f.close()
 
 
 
